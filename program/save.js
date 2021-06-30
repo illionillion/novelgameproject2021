599a0175ab2a999a7beda8b0ce83fb7947d0c5bd
illionillion <yuto.0526.sano@gmail.com>
2021-07-01 07:47:43 +0900

    各テキストのエフェクト追加や、セーブからロードへのやり方が難しそうだ。

--- program/save.js
+++ program/save.js
@@ -4,19 +4,34 @@ console.log(save_file);
 
 function change_save_screen(){
   document.querySelector("#save_screen").classList.toggle("none");
+  // document.querySelector("#save_screen").addEventListener();
+  // for(let i=0;i<3;i++){
+  //   document.getElementsByClassName("option_count")[i].addEventListener("click",function(){save(this)},false);
+  // }
 }
 
 function save(index){
+
   console.log(save_data);
   var index_data=index.getAttribute("index");
+  var index_text_data=index.getAttribute("text");
   console.log(index_data);
-  var save_data2=new Object;
-  //同じオブジェクトを代入できない
-  for(let i=0;i<Object.keys(save_data).length;i++){
-    save_data2[Object.keys(save_data)[i]]=save_data[Object.keys(save_data)[i]];
+
+  var flag=window.confirm(index_text_data+"にセーブしますか？");
+
+  if(flag){
+    var save_data2=new Object;
+    //同じオブジェクトを代入できない
+    for(let i=0;i<Object.keys(save_data).length;i++){
+      save_data2[Object.keys(save_data)[i]]=save_data[Object.keys(save_data)[i]];
+    }
+
+    save_file[index_data]=save_data2;
+
+    console.log(save_file);
+    
+    alert("セーブしました");
   }
 
-  save_file[index_data]=save_data2;
 
-  console.log(save_file);
 }
