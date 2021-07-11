29d34e073224297261dc1e87692a25e44f1fffaf
illionillion <yuto.0526.sano@gmail.com>
2021-07-12 07:11:46 +0900

    ローカルファイルだとjsonの読み込みができないのでCD-ROM版は難しい、解決法としてローカルホスト時とローカルファイルで分岐してjsonかjsonデータが入ってあるjsファイルかで分けて読み込ませる方式になった。めんどくさいからjsファイルにまとめるか考え中。新規テキスト追加。一つエンドができたのでそろそろエンドロールの作業に取り掛かろうと思う。

--- program/text_data.js
+++ program/text_data.js
@@ -42,52 +42,60 @@ var rand_name=encrypted;
 
 var text_data={};
 
+function change_json_name(data){
+  var len = Object.keys(data).length;
+  // ulObj = $("#demo"),
+  
+  // console.log(data);
+  // console.log(len);
+  for(var i = 0; i < len; i++) {
+
+  // console.log(Object.keys(data)[i]);
+  let input_data=data[Object.keys(data)[i]]["text_data"];
+  let len2=input_data.length;
+  // console.log(input_data);
+  // console.log(len2);
+
+  for (let j=0;j<len2;j++){
+    // console.log(input_data[j]);
+    let replace_rand_name=input_data[j]["name"];
+    let replace_rand_text=input_data[j]["text"];
+
+    let replaced_rand_name=replace_rand_name.replace("rand_name",rand_name);
+    let replaced_rand_text=replace_rand_text.replace("rand_name",rand_name);
+
+    while(replace_rand_text!==replaced_rand_text){
+
+      replace_rand_text=replace_rand_text.replace("rand_name",rand_name);
+      replaced_rand_text=replace_rand_text.replace("rand_name",rand_name);
+
+    }
+
+
+    input_data[j]["name"]=replaced_rand_name;
+    input_data[j]["text"]=replaced_rand_text;
+
+    }
+  }
+  add_propaty(data);
+  console.log(data);
+
+  text_data=data;
+  all_num=text_data[now_page]['text_data'].length;
+  audio_file_path=text_data[now_page]['audio']['file_path'];
+
+}
+
 //jsonファイルから読み取る
 $(function() {
-  $.getJSON("json/text_data.json" , function(data) {
-    var len = Object.keys(data).length;
-      // ulObj = $("#demo"),
-      
-    // console.log(data);
-    // console.log(len);
-    for(var i = 0; i < len; i++) {
 
-      // console.log(Object.keys(data)[i]);
-      let input_data=data[Object.keys(data)[i]]["text_data"];
-      let len2=input_data.length;
-      // console.log(input_data);
-      // console.log(len2);
-
-      for (let j=0;j<len2;j++){
-        // console.log(input_data[j]);
-        let replace_rand_name=input_data[j]["name"];
-        let replace_rand_text=input_data[j]["text"];
-
-        let replaced_rand_name=replace_rand_name.replace("rand_name",rand_name);
-        let replaced_rand_text=replace_rand_text.replace("rand_name",rand_name);
-
-        while(replace_rand_text!==replaced_rand_text){
-
-          replace_rand_text=replace_rand_text.replace("rand_name",rand_name);
-          replaced_rand_text=replace_rand_text.replace("rand_name",rand_name);
-
-        }
+  if(location.hostname != ""){
+    $.getJSON("json/text_data.json" , function(data) {
+        change_json_name(data);
+    });
+  }else{
+    change_json_name(substitute_text_data);
+  }
 
 
-        input_data[j]["name"]=replaced_rand_name;
-        input_data[j]["text"]=replaced_rand_text;
-
-        // console.log(replace_rand_name);
-        // console.log(replace_rand_text);
-        // console.log(replaced_rand_name);
-        // console.log(replaced_rand_text);
-      }
-    }
-    add_propaty(data);
-    console.log(data);
-    
-    text_data=data;
-    all_num=text_data[now_page]['text_data'].length;
-    audio_file_path=text_data[now_page]['audio']['file_path'];
-  });
 });
