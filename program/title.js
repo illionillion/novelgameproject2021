0fc491b7c44b73fe435756943ca5b5ac75474118
illionillion <yuto.0526.sano@gmail.com>
2021-08-21 20:27:55 +0900

    音声再生の修正。

--- program/title.js
+++ program/title.js
@@ -3,7 +3,7 @@ $('#view_start').click(function(){
   $('#title_btn_box').removeClass('none');
   $('#user_frame').removeClass('none');
   const title_music="audio/Datura_inst.m4a";
-  audio_file_path=title_music;
-  console.log(audio_file_path);
-  audio_start();
+  music_file=new Audio(title_music);
+  music_file.play();
+  music_file.loop=true;
 })
