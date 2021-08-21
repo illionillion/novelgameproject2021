1dba964ff30ecd5e72aa12f8edb0467b0b46b21b
illionillion <yuto.0526.sano@gmail.com>
2021-08-21 11:23:10 +0900

    タイトル画面をクリックしてから音楽流すようにした。これからいろいろ変えてく

--- program/title.js
+++ program/title.js
@@ -1,8 +1,17 @@
 window.onload=function(){
 
   const title_music="audio/Datura_inst.m4a";
-  music_file=title_music;console.log(music_file);
-  // audio_start();
-  music_file=new Audio(title_music);
-  music_file.play();
+  music_file_path=title_music;console.log(music_file_path);
+  setTimeout(() => {
+    // audio_start();
+    // music_file=new Audio(title_music);
+    // music_file.play();
+  }, 1500);
 }
+
+$('#title_frame').click(function(){
+  $('#title_btn').removeClass('none');
+  const title_music="audio/Datura_inst.m4a";
+  music_file_path=title_music;console.log(music_file_path);
+  audio_start();
+})
